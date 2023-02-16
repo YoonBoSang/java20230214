9de8dbd6208f73f17303965100816d2ffdca2e3f
BoSang Yoon <qhtkddl002@naver.com>
2023-02-16 17:34:36 +0900

    23-02-16

---
+++ src/Test/Test2.java
@@ -0,0 +1,23 @@
+package Test;
+
+public class Test2 {
+	public static void main(String[] args) {
+		 max mm = new max();
+		 long result = mm.higher(3, 5);
+		 System.out.println(result);
+	}
+}
+
+class max {
+	int higher(int a, int b) {
+//		if (a > b) {
+//			return a;
+//		} else {
+//			return b;
+//		}
+		return a > b ? a : b;
+	};
+	int lower(int a, int b) {
+		return a > b ? b : a;
+	}
+}
