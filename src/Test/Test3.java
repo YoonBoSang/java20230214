71b4848729e1e077aa338a40cca81cf5d6a6a73a
BoSang Yoon <qhtkddl002@naver.com>
2023-02-20 09:10:31 +0900

    Update Test3.java

--- src/Test/Test3.java
+++ src/Test/Test3.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Test3 {
 	public static void main(String[] args) {
 		String input = """
-				71
+				26
 				""";
 		
 		Scanner scanner = new Scanner(input);
@@ -14,25 +14,19 @@ public class Test3 {
 		int NC = N;
 		int num = 0;
 		int sum = 0;
-		for (int i = 0; N == num; i++) {
-			int FC = NC / 10 + NC % 10;  		//10의자리 1의자리 더한값 넣기
-			num = (NC % 10) * 10 + FC % 10;		//양변 첫번째 자리 합치기
-			NC = num;
-			System.out.println(N);
-			System.out.println(num);
-			
-			System.out.println(i);
-		}
-//		do {
-//		int FC = N / 10 + N % 10;  			//10의자리 1의자리 더한값 넣기
-//		num = (N % 10) * 10 + FC % 10;		//양변 첫번째 자리 합치기
-////		NC = num;
-//		sum += 1;
-//		System.out.println(NC);
-//		System.out.println(num);
-//		System.out.println(N);
-//		
-//		} while (N == num);
+//		for (int i = 0; i != N; sum++) {
+//			int FC = NC / 10 + NC % 10;  		//10의자리 1의자리 더한값 넣기
+//			i = ((NC % 10) * 10) + FC % 10;		//양변 첫번째 자리 합치기
+//			NC = i;
+//			}
 //		System.out.println(sum);
+		do {
+		int FC = NC / 10 + NC % 10;  			//10의자리 1의자리 더한값 넣기
+		num = (NC % 10) * 10 + FC % 10;		//양변 첫번째 자리 합치기
+		NC = num;
+		sum += 1;
+		} while (N != num);
+		System.out.println(sum);
 	}
-}
+	}
+
