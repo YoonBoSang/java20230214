5eb90b823020f6bd42b67952859dc23d0afa297d
BoSang Yoon <qhtkddl002@naver.com>
2023-02-24 17:53:24 +0900

    23-02-24

---
+++ src/Test/Test20.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class Test20 {
+	public static void main(String[] args) {
+		int n = 144;
+        int answer = 0;
+        for(int i = 1; i <= 1000; i++) {
+            if(n % i == 0) {
+                System.out.println(1);
+            }
+            System.out.println(2);
+        }
+    }
+	}
+
---
+++ src/Test/Test21.java
@@ -0,0 +1,16 @@
+package Test;
+
+public class Test21 {
+	public static void main(String[] args) {
+		String my_string =  "hi12392";
+        int[] answer = null;
+        String str = my_string.replaceAll("[a-z]", "");
+        String[] arr = str.split("");
+        for (int i = 0; i < arr.length; i++) {
+            answer = new int[arr.length];
+            answer[i] = Integer.parseInt(arr[i]);
+        }
+        System.out.println(answer[1]);
+        
+	}
+}
